b5114e8c76a7a326e4e451bf934e7eca623d84ce
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-18 16:57:19 -0600

    Update validation methods to take a Rider array instead of a Ride

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,16 +1,16 @@
 import { Family, Sex } from './model';
-import type { Ride, Rider } from './model';
+import type { Rider } from './model';
 
-export function isValidRide(ride: Ride): boolean {
-	return ride.riders.some((r) => r.family === Family.Thompson && r.age < 18 && r.sex === Sex.Female)
-		? ride.riders.some(canChaperoneThompsonGirls)
-		: ride.riders.some((r) => r.age >= 18);
+export function isValidRide(riders: Rider[]): boolean {
+	return riders.some((r) => r.family === Family.Thompson && r.age < 18 && r.sex === Sex.Female)
+		? riders.some(canChaperoneThompsonGirls)
+		: riders.some((r) => r.age >= 18);
 }
 
 export function canChaperoneThompsonGirls(rider: Rider): boolean {
 	return rider.age >= 18 && rider.sex === Sex.Female;
 }
 
-export function isKidRide(ride: Ride): boolean {
-	return ride.riders.some((r) => r.age < 18);
+export function isKidRide(riders: Rider[]): boolean {
+	return riders.some((r) => r.age < 18);
 }
