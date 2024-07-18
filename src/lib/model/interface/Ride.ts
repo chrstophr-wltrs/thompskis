3fa9a06dd4b1affd8ecc146be8837afef6f34693
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-18 16:23:18 -0600

    Add duration field

--- src/lib/model/interface/Ride.ts
+++ src/lib/model/interface/Ride.ts
@@ -1,9 +1,11 @@
+import type { Duration } from 'date-fns';
 import type { Rider } from '.';
 
 export interface Ride {
 	id: string;
 	startTime: Date;
 	endTime: Date | null;
+	duration: Duration | null;
 	isComplete: boolean;
 	riders: Rider[];
 }
