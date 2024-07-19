0424ba99d11bcd0e094b4e816f83da2cf70b3013
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-19 16:29:20 -0600

    Create WaitlistService

---
+++ src/lib/WaitlistService.ts
@@ -0,0 +1,66 @@
+import type { Rider, WaitlistEntry } from './model';
+import { isKidRide } from './validation';
+
+export default class WaitlistService {
+	private static _waitlist: WaitlistEntry[] = [];
+
+	public static get(searchString?: string): WaitlistEntry[] {
+		return searchString
+			? WaitlistService._waitlist.filter((entry) =>
+					entry.riders.some(
+						(rider) =>
+							rider.firstName.includes(searchString) ||
+							rider.lastName.includes(searchString) ||
+							(rider.nickName && rider.nickName.includes(searchString))
+					)
+				)
+			: WaitlistService._waitlist;
+	}
+
+	public static add(riders: Rider[]): WaitlistEntry {
+		const entry: WaitlistEntry = {
+			id: crypto.randomUUID(),
+			createdAt: new Date(),
+			riders: riders,
+			rideCountScore: WaitlistService.findRideCountScore(riders),
+			rideMinutesScore: WaitlistService.findRideMinutesScore(riders),
+			expectedWaitMinutes: 0
+		};
+		WaitlistService._waitlist.push(entry);
+		return entry;
+	}
+
+	private static findRideCountScore(riders: Rider[]): number {
+		if (isKidRide(riders)) {
+			const kidRiders = riders.filter((rider) => !rider.isAdult);
+			return kidRiders.reduce((acc, rider) => acc + rider.rideCount, 0) / kidRiders.length;
+		}
+		return riders.reduce((acc, rider) => acc + rider.rideCount, 0) / riders.length;
+	}
+
+	private static findRideMinutesScore(riders: Rider[]): number {
+		if (isKidRide(riders)) {
+			const kidRiders = riders.filter((rider) => !rider.isAdult);
+			return kidRiders.reduce((acc, rider) => acc + rider.rideMinutes, 0) / kidRiders.length;
+		}
+		return riders.reduce((acc, rider) => acc + rider.rideMinutes, 0) / riders.length;
+	}
+
+	public static remove(entryId: string): void {
+		WaitlistService._waitlist = WaitlistService._waitlist.filter((entry) => entry.id !== entryId);
+	}
+
+	public static removeByRider(riderId: string): void {
+		WaitlistService._waitlist = WaitlistService._waitlist.filter(
+			(entry) => !entry.riders.some((rider) => rider.id !== riderId)
+		);
+	}
+
+	public static update(entryId: string, riders: Rider[]): WaitlistEntry | undefined {
+		const entry = WaitlistService._waitlist.find((entry) => entry.id === entryId);
+		if (entry) {
+			entry.riders = riders;
+			return entry;
+		}
+	}
+}
