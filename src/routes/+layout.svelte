9395d3cb559171bf62d4fce257a7e16ff71cb131
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-22 14:31:24 -0600

    Properly style navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,8 @@
-<script>
-	import '../app.css';
-</script>
-
 <div class="navbar bg-base-200">
 	<div class="mx-auto space-x-4">
-		<a class="btn bg-base-100 text-xl text-base-content" href="/">Waitlist</a>
-		<a class="btn bg-base-100 text-xl text-base-content" href="/rides">History</a>
-		<a class="btn bg-base-100 text-xl text-base-content" href="/riders">Riders</a>
+		<a class="btn text-xl bg-base-100 text-base-content" href="/">Waitlist</a>
+		<a class="btn  text-xl bg-base-100 text-base-content" href="/rides">History</a>
+		<a class="btn  text-xl bg-base-100 text-base-content" href="/riders">Riders</a>
 	</div>
 </div>
 
