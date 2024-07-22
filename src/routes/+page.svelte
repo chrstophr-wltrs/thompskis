414c224323df9d81e0e58620dfaa5d56954fb797
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-22 15:07:28 -0600

    Create multiple test rides

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import RideCard from '$lib/components/RideCard.svelte'; // Import the RideCard component
-	let testRide = {
+	let testRide1 = {
 		riders: [{ nickName: 'Alice' }, { nickName: 'Bob' }, { nickName: 'Charlie' }],
+		duration: 10
+	};
+	let testRide2 = {
+		riders: [{ nickName: 'Danny' }, { nickName: 'Ellie' }, { nickName: 'Frank' }],
 		duration: 10
 	};
 </script>
 
-<div class="container mx-auto px-4 prose">
-	<h1>ThompSkis</h1>
-	<RideCard ride={testRide} />
+<div class="container mx-auto px-4 space-y-4 prose">
+	<RideCard ride={testRide1} />
+	<RideCard ride={testRide2} />
 </div>
