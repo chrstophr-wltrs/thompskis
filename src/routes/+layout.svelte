ebf7c58e9126c4a48e0893a4bb14593f5dc421db
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-22 15:05:53 -0600

    Add margin after navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<div class="navbar bg-base-200">
+<div class="navbar bg-base-200 mb-4">
 	<div class="mx-auto space-x-4">
 		<a class="btn text-xl bg-base-100 text-base-content" href="/">Waitlist</a>
 		<a class="btn  text-xl bg-base-100 text-base-content" href="/rides">History</a>
