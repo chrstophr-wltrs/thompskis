dd5cb93d69cf05320849eec5a20a816394058d24
Chris Walters <chrstophr.wltrs@gmail.com>
2024-07-22 14:19:44 -0600

    Add navbar with buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.css';
 </script>
 
+<div class="navbar bg-base-200">
+	<div class="mx-auto space-x-4">
+		<a class="btn bg-base-100 text-xl text-base-content" href="/">Waitlist</a>
+		<a class="btn bg-base-100 text-xl text-base-content" href="/rides">History</a>
+		<a class="btn bg-base-100 text-xl text-base-content" href="/riders">Riders</a>
+	</div>
+</div>
+
 <slot />
